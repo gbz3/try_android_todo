59f0a8fac4cee819576cd4f3164558f1e4c5d725
gbz3 <1808483+gbz3@users.noreply.github.com>
2021-07-11 20:03:49 +0900

    3rd commit

---
+++ app/src/main/java/com/example/try_android_todo/ui/detail/DetailFragment.kt
@@ -0,0 +1,24 @@
+package com.example.try_android_todo.ui.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.try_android_todo.R
+
+class DetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = DetailFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.detail_fragment, container, false)
+    }
+
+}
--- app/src/main/java/com/example/try_android_todo/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/try_android_todo/ui/main/MainFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.try_android_todo.R
 
 class MainFragment : Fragment() {
@@ -27,6 +30,16 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navHostFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            navController.navigate(R.id.action_mainFragment_to_detailFragment)
+        }
     }
 
 }
